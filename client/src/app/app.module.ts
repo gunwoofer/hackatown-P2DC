1a16f29199a5ef53ae7655a9a5946ebd6cacf156
Anas jmii <anasjmii@gmail.com>
2018-01-20 17:46:00 -0500

    google map

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
+import { AgmCoreModule } from '@agm/core';
 
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { MapLocationComponent } from './map-location/map-location.component';
+
 
 
 @NgModule({
@@ -15,11 +18,15 @@ import { WelcomeComponent } from './welcome/welcome.component';
     AppComponent,
     HomeComponent,
     LoginComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    MapLocationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCuk_evPYxWlWPJ3PrR0Mc_IBhdxGmMNG0'
+    }),
     HttpModule
   ],
   providers: [
