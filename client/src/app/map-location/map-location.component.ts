70cbbce5d4017d7628f1ee27302b9b88fb3fa29c
Anas jmii <anasjmii@gmail.com>
2018-01-20 17:45:34 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ client/src/app/map-location/map-location.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-location',
+  templateUrl: './map-location.component.html',
+  styleUrls: ['./map-location.component.css']
+})
+export class MapLocationComponent {
+  public latitude: number = 51.678418;
+  public longitude: number = 7.809007;
+}
