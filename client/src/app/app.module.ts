7203244f651424565eb7ebd62111cc7b5a15ae61
DanielPopOut <stephane.tchangang@gmail.com>
2018-01-20 16:48:03 -0500

    Ajout module routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpModule
   ],
   providers: [
