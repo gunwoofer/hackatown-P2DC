9877011033618008545f23af432e5cb4097e8ca4
DanielPopOut <stephane.tchangang@gmail.com>
2018-01-20 18:52:27 -0500

    bouton rouge

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { LoginComponent } from './../login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-  { path: '', component: WelcomeComponent },
-  { path : 'login', component: LoginComponent},
-  { path: 'home', component: HomeComponent }
+  {path: '', component: WelcomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
