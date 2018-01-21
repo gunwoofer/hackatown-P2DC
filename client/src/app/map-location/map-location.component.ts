d3a55bcdc9354b7527a39cc9d852cf4eeadb8815
Valentin Bouis <valentin.bouis@gmail.com>
2018-01-21 01:35:12 -0500

    location du challenge selectionne

--- client/src/app/map-location/map-location.component.ts
+++ client/src/app/map-location/map-location.component.ts
@@ -1,20 +1,22 @@
+import { Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+
+declare var google;
 
 @Component({
   selector: 'app-map-location',
   templateUrl: './map-location.component.html',
   styleUrls: ['./map-location.component.css']
 })
+
 export class MapLocationComponent implements OnInit {
 
     public adresse: string;
     public latitude: number;
     public longitude: number;
-    public locationChosen: boolean;
 
-    constructor(private route: ActivatedRoute) {
-
+    constructor(private router: Router, private route: ActivatedRoute, private http: Http) {
     }
 
     public ngOnInit(): void {
@@ -22,17 +24,21 @@ export class MapLocationComponent implements OnInit {
             this.adresse = params['adresse'];
          });
          console.log(this.adresse);
-
-        this.latitude = 51.678418;
-        this.longitude = 7.809007;
-        this.locationChosen = false;
+        const self = this;
+        const geocoder = new google.maps.Geocoder();
+        geocoder.geocode({'address': this.adresse}, function(results, status) {
+            if (status === 'OK') {
+                console.log(results[0].geometry.location.lat());
+                console.log(results[0].geometry.location.lng());
+                self.latitude = results[0].geometry.location.lat();
+                self.longitude = results[0].geometry.location.lng();
+              } else {
+              alert('Geocode was not successful for the following reason: ' + status);
+            }
+        });
     }
 
-
-    public onChoseLocation(event): void {
-        console.log(event);
-        this.latitude = event.coords.lat;
-        this.longitude = event.coords.lng;
-        this.locationChosen = true;
+    public back(): void {
+        this.router.navigateByUrl('/liste');
     }
 }
